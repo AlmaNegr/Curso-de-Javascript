94990ae872c29a5bde6886b4ed67c3131589881c
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-18 05:21:29 -0300

    treino

--- Aula/script.js
+++ Aula/script.js
@@ -1,22 +1,21 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var nasc = document.getElementById('txtano').value
     var res = document.querySelector('div#res')
-
-    if (fano.value.length == 0 || Number(fano.value) < ano) {
-        alert('Verifique os dados e tente novamente.')
-        //Impede que os dados sejam colocados incorretamente.
-    }
-    var fsex = document.getElementsByName('radsex')
-    var idade = ano - Number(fano.value)
-    var genero = ''
-    var img = document.createAttribute('img')
-    img.setAttribute('src', 'foto')
-
-    if (fsex[0].checked) {
-        genero = 'homem'
-        if (idade >= 0 && idade < 10) {
-            img.setAttribute('src', 'bebe-menino.png')
+    if (nasc.length == 0 || Number(nasc) > ano) {
+        alert('[Erro] verifique os dados e tente novamente.')
+    } else {
+        var sex = document.getElementsByName('radsex')
+        
+        var idade = ano - Number(nasc)
+        var genero = ''
+        if (sex[0].checked) {
+            genero = 'Homem'
+        } else if (sex[1].checked) {
+            genero = 'Mulher'
         }
-
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+    }
+}
