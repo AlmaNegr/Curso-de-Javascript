1f96aa6ae722ba6a0636b921742637d8b6fa1015
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-19 05:20:10 -0300

    treino de javascript

--- Aula/script.js
+++ Aula/script.js
@@ -3,46 +3,23 @@ function verificar() {
     var ano = data.getFullYear()
     var nasc = document.getElementById('txtano').value
     var res = document.querySelector('div#res')
-    if (nasc.length == 0 || Number(nasc) > ano) {
-        alert('[Erro] verifique os dados e tente novamente.')
+    if (nasc.length == 0 || nasc > ano) {
+        alert('[ERRO] preencha os dados corretamente!')
     } else {
-        var sex = document.getElementsByName('radsex')
-        var idade = ano - Number(nasc)
+        var sexo = document.getElementsByName('radsex')
+        var idade = ano - nasc
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        if (sex[0].checked) {
+        if (sexo[0].checked) {
             genero = 'Homem'
-            if (idade >= 0 && idade < 11) {
+            if (idade < 11) {
                 //Criança
                 img.setAttribute('src', 'bebe-menino.png')
-            } else if (idade < 18) {
-                //Jovem
-                img.setAttribute('src', 'jovem-homem.png')
-            } else if (idade < 60) {
-                //Adulto  
-                img.setAttribute('src', 'homem-adulto.png')
-            } else {
-                //Idoso
-                img.setAttribute('src', 'idoso-m.png')
             }
-        } else if (sex[1].checked) {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 11) {
-                //Criança
-                img.setAttribute('src', 'bebe-menina.png')
-            } else if (idade < 18) {
-                //Jovem
-                img.setAttribute('src', 'jovem-mulher.png')
-            } else if (idade < 60) {
-                //Adulto
-                img.setAttribute('src', 'mulher-adulta.png')
-            } else {
-                //Idoso
-                img.setAttribute('src', 'idosa-m.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
-    }   res.appendChild(img)
+        } 
+    }
+     res.style.textAlign = 'center'
+     res.innerHTML = `Detectamos ${genero} com ${idade} anos de idade`
+     res.appendChild(img)
 }
