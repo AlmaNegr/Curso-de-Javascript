e6341baf6a31b4c9ed8e2a9f238323ec63b71ac6
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-23 12:37:29 -0300

    repetiçãoo

--- aula14ex/exe016/script.js
+++ aula14ex/exe016/script.js
@@ -3,9 +3,18 @@ function contar() {
     var f = Number(document.getElementById('f').value)
     var p = Number(document.getElementById('p').value)
     var res = document.querySelector('div#res')
-    res.innerHTML = `O inicio é ${i}, o fim é ${f} e o passo é ${p}.`
-    for (i;i<=f;p) {
-        console.log(i)
+    if (isNaN(i) || isNaN(f) || isNaN(p)) {
+        res.innerHTML = 'Por favor, insira valores válido.'
+        return;
+    }
+    if (p < 0) {
+        res.innerHTML = 'Passo inválido! Considere um valor maior que 0.'
+        return;
+    }
+    if (i < f) {
+        for (var cont = i; cont <= f;cont += p) {
+            res.innerHTML += `${cont} \u{1f449}`
+        }
 
     }
 }
