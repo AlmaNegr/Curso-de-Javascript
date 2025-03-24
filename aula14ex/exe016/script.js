df883f525af9282e584f0993a4b636e3d19da054
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-24 17:37:02 -0300

    exercicio
    exercício de repetição

--- aula14ex/exe016/script.js
+++ aula14ex/exe016/script.js
@@ -1,22 +1,7 @@
 function contar() {
-    var i = Number(document.getElementById('i').value)
-    var f = Number(document.getElementById('f').value)
-    var p = Number(document.getElementById('p').value)
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var pas = document.getElementById('txtp')
     var res = document.querySelector('div#res')
-    res.innerHTML = ''
-    if (isNaN(i) || isNaN(f) || isNaN(p) || i === 0 && f === 0 ) {
-        res.innerHTML = 'Por favor, insira valores válido.'
-        return;
-    }
-    if (p <= 0) {
-        res.innerHTML = 'Passo inválido! Considere um valor maior que 0.'
-        p = 1
-        return;
-    }
-    if (i < f) {
-        for (var cont = i; cont <= f;cont += p) {
-            res.innerHTML += `${cont} \u{1f449}`
-        }
 
-    }
-}
+}   if ()
