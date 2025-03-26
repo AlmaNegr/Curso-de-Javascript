f89b9b7bcdfb34d352f1819c32ac2641892d01fd
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-26 17:28:17 -0300

    tabuada

--- aula14ex/exe017/script.js
+++ aula14ex/exe017/script.js
@@ -1,1 +1,16 @@
-window.alert('Olá')
+function tabuada() {
+    var n = document.getElementById('n1')
+    var res = document.querySelector('div#res')
+    var num = Number(n.value)
+    
+    if (isNaN(num) || n.value.length == 0) {
+        alert('Dados inválidos!')
+        
+    } else {
+        res.innerHTML = ''
+        for (c = num;c <= 10;c += 1) {
+            res.innerHTML += `<br>${num}x${c} = ${num*c}`
+        }
+            
+    }
+}
