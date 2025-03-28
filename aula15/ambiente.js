ba219848825b68d47fe3483e4fff301af305763a
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-28 17:40:33 -0300

    vetor

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+var  num = [1, 4, 3, 0]
+//console.log(num)
+num.push(5)
+console.log(`Nosso vetor em ordem crescente...`)
+console.log(num.sort())
+console.log(`Nosso vetor tem ${num.length} elementos.`)
