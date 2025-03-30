478118b6141f5ba13b47a2122f290f9930eedb6d
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-03-30 10:58:30 -0300

    função

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+//var res = parimp(2)
+console.log(parimp(4)) // Se quiser colocar a chamada dentro logo do console.log tambem pode.
---
+++ aula16/funcao03.js
@@ -0,0 +1,11 @@
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
+
+let a = function(y) {
+    return y / 2
+}
+
+console.log(parseFloat(a(5)))
